eac00638d41f2226da9516a47e28a101f49bd56d
John <7268419+Pattern-Projects@users.noreply.github.com>
2019-06-12 18:23:42 +0100

    retrieve from api

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("main.js loaded");
+var xhr = new XMLHttpRequest();
+
+xhr.open("GET", "https://api.oireachtas.ie/v1/debates");
+xhr.send();
+
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        console.log(JSON.parse(this.responseText));
+    }
+}
