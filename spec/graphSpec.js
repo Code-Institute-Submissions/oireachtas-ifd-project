c871b6d7741271105089dedfd565249b25278b44
John <7268419+Pattern-Projects@users.noreply.github.com>
2019-06-18 22:49:31 +0100

    retrieving data from API

--- spec/graphSpec.js
+++ spec/graphSpec.js
@@ -1,7 +1,10 @@
-describe("Graphs", function () {
-    describe("retrieveDebates", function () {
-        it("should retun undefined", function () {
-            expect(retrieveDebates()).toBe(undefined);
+describe("Member Page", function () {
+    describe("retrieveMember", function () {
+        it("should return nothing", function () {
+            expect(retrieveMember()).toBe();
         });
+        it("should call queue", function () {
+            expect();
+        })
     });
 });
