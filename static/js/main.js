9d15396ddf3b0a48e6383786db28b3d8f4701d00
John <7268419+Pattern-Projects@users.noreply.github.com>
2019-06-25 13:34:13 +0100

    index needs merge

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,6 @@
+var current;
+
+
 function oireachtasPage () {
     clearPage();
     var data = document.getElementById("data");
@@ -6,9 +9,54 @@ function oireachtasPage () {
 }
 
 function memberPage () {
+    retrieveMember();
     clearPage();
     var data = document.getElementById("data");
+<<<<<<< Updated upstream
     data.innerHTML = `<h1>Member</h1>`;
+=======
+    data.innerHTML = `
+    <h1>Michael Ahern</h1>
+    <p>${current}</p>
+    <div class="row">
+        <div class="col-3">
+            <img src="https://data.oireachtas.ie/ie/oireachtas/member/id/Se%C3%A1n-%C3%93-Feargha%C3%ADl.S.2000-06-09/image/large" alt="" class="member-img rounded">
+        </div>
+        <div class="col-6">
+            <p>TD in the Dail</p>
+            <p>Representing Cork</p>
+            <p>Since 21/05/14</p>
+        </div>
+    </div>
+
+    <h2>Sponsored Bills</h2>
+    <div class="list-group">
+        <a onclick="legislationPage()" href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+            <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">Health (Pricing and Supply of Medical Goods) (Amendment) Bill 2018: First Stage</h5>
+                <small>Dail</small>
+            </div>
+            <small>Status: Current</small>
+        </a>
+        <a onclick="legislationPage()" href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                <div class="d-flex w-100 justify-content-between">
+                    <h5 class="mb-1">Health (Pricing and Supply of Medical Goods) (Amendment) Bill 2018: First Stage</h5>
+                    <small>Dail</small>
+                </div>
+                <small>Status: Current</small>
+        </a>
+        <a onclick="legislationPage()" href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+            <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">Health (Pricing and Supply of Medical Goods) (Amendment) Bill 2018: First Stage</h5>
+                <small>Dail</small>
+            </div>
+            <small>Status: Current</small>
+        </a>
+    </div>
+    
+
+    `;
+>>>>>>> Stashed changes
     return "member";
 }
 
@@ -27,6 +75,7 @@ function retrieveMember () {
 
 function drawMember (error, data) {
     console.log(data);
+    current = data;
 }
 
 function clearPage () {
