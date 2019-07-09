433b4b1f38ac52cc834924727d4526f00f6a36ba
John <7268419+Pattern-Projects@users.noreply.github.com>
2019-07-09 10:21:53 +0100

    Add licence to pages

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,20 @@
+//  A data repository for displaying data gathered from oireachtas API.
+//    Copyright (C) <2019>  <John O' Sullivan
+
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses. 
+
+
 // Pagination Object - Used for paging through members and bills on front page
 class Pagination {
     constructor(tag) {
